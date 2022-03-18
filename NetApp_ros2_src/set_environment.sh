bf1226e5293a5b7fdded0c8e2954ed310e53e05b
Jakub Špaňhel <ispanhel@fit.vutbr.cz>
2022-03-18 22:59:59 +0100

    get own location as root

--- NetApp_ros2_src/set_environment.sh
+++ NetApp_ros2_src/set_environment.sh
@@ -1,3 +1,5 @@
-ASSETS_PATH=$(realpath assets/)
+#!/bin/bash
+ROOT_PATH=$(dirname "$BASH_SOURCE")
+ASSETS_PATH="$ROOT_PATH/assets/"
 export ROS2_5G_ERA_ASSETS_PATH=$ASSETS_PATH
 echo "ROS2_5G_ERA_ASSETS_PATH set to $ASSETS_PATH"
